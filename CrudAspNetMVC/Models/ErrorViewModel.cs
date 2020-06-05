6a884dbcd0401926aacc2347347226c6a0543d7b
Luiz9k <luiz.xaxim.95@gmail.com>
2020-06-05 18:33:11 -0300

    Adicionar arquivos de projeto.

---
+++ CrudAspNetMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CrudAspNetMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
